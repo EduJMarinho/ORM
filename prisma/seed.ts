cf349d2c8999cab2beda02aaf250b04ce93f1e07
Edu-Marinho <marinhojcc@hotmail.com>
2025-08-27 15:30:04 -0300

    Utilização do Prisma ORM

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/prisma";
+
+async function seed() {
+    await prisma.user.createMany({
+        data: [
+
+            {
+                name: "Mayk Brito",
+                email: "maik@email.com"
+            },
+            {
+                name: "Diego Fernandes",
+                email: "diego@email.com"
+            },
+
+        ]
+    })
+
+}
+seed().then(() => {
+    console.log("Database seeded!")
+    prisma.$disconnect()
+})
